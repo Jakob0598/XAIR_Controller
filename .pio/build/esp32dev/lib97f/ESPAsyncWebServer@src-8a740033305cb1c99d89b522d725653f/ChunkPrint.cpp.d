e2ea62cdae9ca2a13097862ce2b35288bd6a2eaa
Jakob0598 <gerlach.jakob@gmx.de>
2026-03-14 21:41:29 +0100

    ignore .pio folder

--- .pio/build/esp32dev/lib97f/ESPAsyncWebServer@src-8a740033305cb1c99d89b522d725653f/ChunkPrint.cpp.d
+++
@@ -1,7 +0,0 @@
-.pio/build/esp32dev/lib97f/ESPAsyncWebServer@src-8a740033305cb1c99d89b522d725653f/ChunkPrint.cpp.o: \
- .pio/libdeps/esp32dev/ESPAsyncWebServer@src-8a740033305cb1c99d89b522d725653f/src/ChunkPrint.cpp \
- .pio/libdeps/esp32dev/ESPAsyncWebServer@src-8a740033305cb1c99d89b522d725653f/src/ChunkPrint.h \
- C:/Users/jakob/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
- C:/Users/jakob/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
- C:/Users/jakob/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
- C:/Users/jakob/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
--- .pio/build/esp32dev/src/mixer_state.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/src/mixer_state.cpp.o: src/mixer_state.cpp \
- src/mixer_state.h src/config.h
